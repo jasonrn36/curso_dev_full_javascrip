1bf47f0f9c07152563343b3972da2bf22cb30c34
jasonrn36 <jason.rn36@gmail.com>
2025-09-03 15:20:28 -0400

    executei minha primeira tarefa do grunt ensinado pelo Copilot

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,21 @@
 module.exports = function(grunt) {
-  grunt.initConfig({
-    // configurações aqui
+  // Configuração das tarefas
+    grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json')
   });
 
-    // Área da tarefa
-    grunt.registerTask('hello', function() {
-    console.log('Olá, Grunt!');
+  // Aqui ficam as tarefas
+    grunt.registerTask('mensagem', 'Exibe uma mensagem', function() {
+    grunt.log.writeln('Olá Jason, você está aprendendo Grunt!');
     });
-    // fim da área da tarefa
-}
+
+  // Fim da área de tarefas
+
+    //Para executar esse código
+    // abra o terminal no diretório do projeto
+    // e digite no terminal:
+    // o comando abaixo
+    //   grunt mensagem
+
+};
 
